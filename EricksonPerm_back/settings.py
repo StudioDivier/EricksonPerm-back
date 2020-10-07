26d22423452a462a1af9a58e576379b3bb4fef39
StudioDivier <ianfoxstudio@gmail.com>
2020-10-07 18:13:13 +0300

    edit languages in settings

--- EricksonPerm_back/settings.py
+++ EricksonPerm_back/settings.py
@@ -104,7 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
+
+LANGUAGES = [
+    ('ru', ('Russian')),
+
+]
 
 TIME_ZONE = 'UTC'
 
