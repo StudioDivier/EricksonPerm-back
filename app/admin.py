6961cf339c2efddef86133a37bc047d568678307
StudioDivier <ianfoxstudio@gmail.com>
2020-10-12 18:05:20 +0300

    17

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,7 @@ from .models import Trainers, Education, WayWork, Experience
 from .models import Games, Offers, Feeds, Timetables
 
 
+admin.site.register(Feeds)
 admin.site.register(Trainers)
 admin.site.register(Education)
 admin.site.register(WayWork)
