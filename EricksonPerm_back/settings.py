c6c47a1b7121821b89ac4817435793d23aac7949
StudioDivier <ianfoxstudio@gmail.com>
2020-10-12 17:56:53 +0300

    15

--- EricksonPerm_back/settings.py
+++ EricksonPerm_back/settings.py
@@ -146,6 +146,7 @@ STATIC_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 TRAIN_ROOT = os.path.join(BASE_DIR, 'media/coach_detail')
 CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
