0581e80e99261ddb48b3e9ac56c6ad07500e8838
StudioDivier <ianfoxstudio@gmail.com>
2020-10-12 18:20:29 +0300

    20

--- app/models.py
+++ app/models.py
@@ -35,7 +35,7 @@ class ProgramOffers(models.Model):
 
 class Feeds(models.Model):
 
-    img = models.FilePathField(name="img", path=settings.MEDIA_URL, null=True)
+    img = models.FilePathField(name="img", path=settings.BASE_DIR+'/media/feeds', null=True, blank=True)
     title = models.CharField(name="title", max_length=128)
     description_short = models.TextField(name="description_short")  # preview short
     description_long = models.TextField(name="description_long")  # long
